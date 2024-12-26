dea87b2cc745711613eb15a39d1e59bb0cfe44e2
GotadeOrvalho <adrianeferreirap@gmail.com>
2024-12-26 14:44:18 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,17 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "books");
-		
-		
-		
-		Seller sl = new Seller(21, "bob", "bob@gmail.com",  LocalDate.now(),300.0, dp );
 		
 		SellerDao sd = DaoFactory.createSellerDao();
+		
+		Seller sl = sd.findById(3);
 		
 		System.out.println(sl);
 	}
