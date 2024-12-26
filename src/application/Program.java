5b478cfb18205717ab5b8ca44e30231e0ac2c5c0
GotadeOrvalho <adrianeferreirap@gmail.com>
2024-12-26 15:10:31 -0300

    FingByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,6 +18,13 @@ public class Program {
 		Seller sl = sd.findById(3);
 		
 		System.out.println(sl);
+		
+		System.out.println("\n=== TEST 2: Seller findByDepartment===");
+		Department department = new Department(2, null);
+		List<Seller> list = sd.findByDepartment(department);
+		for ( Seller ss : list) {
+			System.out.println(ss);
+		}
 	}
 
 }
